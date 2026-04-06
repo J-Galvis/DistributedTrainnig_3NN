67bd0cffc12ed2b805deba49d37678692399a07e
J_Galvis <galviscamilojuan@gmail.com>
2026-04-06 10:31:21 -0500

    CIFAR10, MNIST update

---
+++ CIFAR10_Distributed_NN/Monolitic/testing.py
@@ -0,0 +1,25 @@
+import torch
+import torchvision.datasets as datasets
+from defineNetwork import Net, TRANSFORM, BATCH_SIZE, NUM_WORKERS
+
+def testingNetwork( testloader, net):
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    return (100 * correct / total)
+
+if __name__ == "__main__":
+    print("starting testing . . . ")
+    testset = datasets.CIFAR10(root='./data', train=False, download=True, transform=TRANSFORM)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=BATCH_SIZE, shuffle=False, num_workers=NUM_WORKERS)
+
+    # Code to initialize testloader and net
+    net = Net()
+    net.load_state_dict(torch.load('./Results/cifar10_trained_model.pth'))
+    print(testingNetwork(testloader, net))
